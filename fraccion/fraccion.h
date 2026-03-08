1501e706b475ea4cc3f9ddaf2be32abd62e30331
JGP-1208 <josgp1208@gmail.com>
2026-03-07 19:09:07 -0600

    Refactor header guards and update function declarations

--- fraccion/fraccion.h
+++ fraccion/fraccion.h
@@ -1,22 +1,12 @@
-#ifndef __FRACCION_H__
-#define __FRACCION_H__
+#ifndef FRACCION_H
+#define FRACCION_H
 
-/* Tipo opaco */
 typedef struct Fraccion Fraccion;
 
-/* Crear una fracción */
 Fraccion* crearFraccion(int num, int den);
-
-/* Simplificar la fracción */
 void simplificar(Fraccion* f);
-
-/* Sumar dos fracciones y regresar una nueva */
-Fraccion* sumar(Fraccion* a, Fraccion* b);
-
-/* Imprimir la fracción */
+Fraccion* sumar(Fraccion* f1, Fraccion* f2);
 void imprimir(Fraccion* f);
-
-/* Liberar memoria */
 void destruir(Fraccion* f);
 
 #endif
