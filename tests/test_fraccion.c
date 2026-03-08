d5651eb7903c9100eb5976ab2f1bc9f3ab132fe0
JGP-1208 <josgp1208@gmail.com>
2026-03-07 19:08:30 -0600

    initial commit
    Refactored test code for Fraccion structure by removing unused functions and adding a main function to test addition and simplification.

--- tests/test_fraccion.c
+++ tests/test_fraccion.c
@@ -1,62 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "Fraccion.h"
+#include "../fraccion/fraccion.h"
 
-struct Fraccion {
-    int num;
-    int den;
-};
+int main()
+{
 
-int calcularMCD(int a, int b) {
-    if (b == 0) return a;
-    return calcularMCD(b, a % b);
-}
+    Fraccion* f1 = crearFraccion(2,4);
+    Fraccion* f2 = crearFraccion(1,2);
 
-void simplificar(Fraccion* f) {
-    if (f == NULL || f->den == 0) return;
+    Fraccion* r = sumar(f1,f2);
 
-    if (f->den < 0) {
-        f->num = -f->num;
-        f->den = -f->den;
-    }
+    if(r == NULL)
+        return 1;
 
-    int mcd = calcularMCD(abs(f->num), abs(f->den));
-    f->num /= mcd;
-    f->den /= mcd;
-}
+    imprimir(r);
 
-Fraccion* crearFraccion(int num, int den) {
-    if (den == 0) {
-        printf("Error: Denominador no puede ser 0.\n");
-        return NULL;
-    }
-    
-    Fraccion* nueva = (Fraccion*)malloc(sizeof(Fraccion));
-    if (nueva != NULL) {
-        nueva->num = num;
-        nueva->den = den;
-        simplificar(nueva);
-    }
-    return nueva;
-}
+    destruir(f1);
+    destruir(f2);
+    destruir(r);
 
-Fraccion* sumar(Fraccion* f1, Fraccion* f2) {
-    if (f1 == NULL || f2 == NULL) return NULL;
-    
-    int nuevoNum = (f1->num * f2->den) + (f2->num * f1->den);
-    int nuevoDen = f1->den * f2->den;
-    
-    return crearFraccion(nuevoNum, nuevoDen);
-}
+    printf("Fraccion OK\n");
 
-void imprimir(Fraccion* f) {
-    if (f == NULL) return;
-    if (f->den == 1) printf("%d\n", f->num);
-    else printf("%d/%d\n", f->num, f->den);
-}
-
-void destruir(Fraccion* f) {
-    if (f != NULL) {
-        free(f);
-    }
+    return 0;
 }
